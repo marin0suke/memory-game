a19e405d1a7785d243280ebfb3412713879dcf71
marin0suke <monk.marina@gmail.com>
2025-02-14 13:50:31 +1000

    Add resetGame function, add handleCardClick function, remove gameOver state variable.

--- src/components/GameLogic/GameLogic.jsx
+++ src/components/GameLogic/GameLogic.jsx
@@ -7,7 +7,6 @@ const GameLogic = () => {
     const [ clickedCards, setClickedCards ] = useState([]);
     const [ currentScore, setCurrentScore ] = useState(0);
     const [ bestScore, setBestScore ] = useState(0);
-    const [ gameOver, setGameOver ] = useState(false);
 
     useEffect(() => {
         const fetchCards = async () => {
@@ -21,9 +20,49 @@ const GameLogic = () => {
                 console.log("Error fetching card data: ", error);
             }
         };
-        
+
         fetchCards();
     }, []) // no dependencies - runs once on mount.
+
+    const shuffleCards = () => { 
+        setCards((prevCards) => {
+            let shuffled = [...prevCards];
+                for (let i = shuffled.length - 1; i > 0; i--) { // decrement 
+                    const j = Math.floor(Math.random() * (i + 1)); // random index
+                    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // swap elements
+                } 
+            return shuffled;
+        })
+    };
+
+    const handleCardClick = (cardId) => {
+        if (clickedCards.includes(cardId)) {
+            resetGame();
+            return;
+        }
+
+        const newScore = currentScore + 1;
+        setClickedCards([...clickedCards, cardId]);
+        setCurrentScore(newScore);
+
+        if (newScore > bestScore) setBestScore(newScore);
+
+        shuffleCards();
+    }
+    // we need handleCardClick to take the card id being clicked, 
+    //first check if that card has been clicked before. if has, game over is called.
+    // if it hasn't then it becomes added to the clickedCard array, - this calls setClickedCards? add all previous cards and add the new card id.
+    // increment the score by one.
+    // if the current score surpasses best score, also update best score.
+
+    const resetGame = () => {
+        setCurrentScore(0);
+        setClickedCards([]);
+        shuffleCards();
+    }
+    // resets the score. resets clickedCards to empty array.
+
+
 
     return (
         <div>
