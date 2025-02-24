8aae866f60e49adeead5c356a13f4b40f82dbf25
marin0suke <monk.marina@gmail.com>
2025-02-24 18:09:21 +1000

    Fix key clash

--- src/components/DisplayCards/DisplayCards.jsx
+++ src/components/DisplayCards/DisplayCards.jsx
@@ -1,16 +1,22 @@
 import Card from "../Card/Card";
 import "./DisplayCards.css";
+import { v4 as uuidv4 } from 'uuid';
 
 const DisplayCards = ({ cards, onCardClick }) => {
     if (!cards || cards.length == 0) {
         return <p>No cards available</p>
     }
 
+    const processedCards = cards.map((card, index) => ({
+        ...card,
+        uniqueId: uuidv4(),
+    }));
+
     return (
         <div className="card-grid">
-        {cards.map((card) => (
+        {processedCards.map((card) => (
             <Card
-            key={card.id}
+            key={card.uniqueId}
             imageUrl={card.imageUrl}
             description={card.description}
             onClick={() => onCardClick(card.id)}
